53ba7f55a8c73e85942fb908f5fc37680066c749
Simone Menichetti <simon3menichetti95@gmail.com>
2024-09-11 14:50:15 +0200

    feat:( add template , add database name in .env , make:model and control page, add route in web.php and insert link in navbar )

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Movie;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+
+        return view('home');
+    }
+
+    public function movies()
+    {
+        $movies = Movie::orderBy('title')->get();
+        return view('movies', compact('movies',));
+    }
+
+
+    public function about()
+    {
+        return view('about');
+    }
+
+    public function contacts()
+    {
+        return view('contacts');
+    }
+}
